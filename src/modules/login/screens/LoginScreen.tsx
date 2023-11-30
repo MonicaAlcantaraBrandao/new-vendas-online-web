4e14ae7ef1b597410dc0d946fe54a917ed5135aa
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-30 19:56:43 -0300

    create login component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,22 @@
-import { BackgroundImage } from "../styles/LoginScreen.styles";
+import SVGLogo from "../../../shared/icons/SVGLogo";
+import {
+  BackgroundImage,
+  ContainerLogin,
+  ContainerLoginScreen,
+  LimitedContainer,
+} from "../styles/LoginScreen.styles";
 
 const LoginScreen = () => {
   return (
     <div>
-      <BackgroundImage src="./background.png" />
+      <ContainerLoginScreen>
+        <BackgroundImage src="./background.png" />
+        <ContainerLogin>
+          <LimitedContainer>
+            <SVGLogo />
+          </LimitedContainer>
+        </ContainerLogin>
+      </ContainerLoginScreen>
     </div>
   );
 };
