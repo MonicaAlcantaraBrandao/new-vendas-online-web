6f4d5adbc3ff234c07395871af3524297031dc7b
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-12-05 21:57:30 -0300

    insert token in request

--- src/shared/hooks/useGlobalContext.tsx
+++ src/shared/hooks/useGlobalContext.tsx
@@ -1,6 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
-
-import { getAuthorizationToken, setAuthorizationToken } from "../functions/connection/auth";
+import { createContext, useContext, useState } from "react";
 
 type NotificationType = "success" | "info" | "warning" | "error";
 
@@ -11,13 +9,12 @@ interface NotificationProps {
 }
 
 interface GlobalData {
-  accessToken?: string;
   notification?: NotificationProps;
 }
 
 interface GlobalContextProps {
   globalData: GlobalData;
-  setGlobalData: (globaldata: GlobalData) => void;
+  setGlobalData: (globalData: GlobalData) => void;
 }
 
 const GlobalContext = createContext({} as GlobalContextProps);
@@ -39,21 +36,6 @@ export const GlobalProvider = ({ children }: GlobalProviderProps) => {
 export const useGlobalContext = () => {
   const { globalData, setGlobalData } = useContext(GlobalContext);
 
-  useEffect(() => {
-    const token = getAuthorizationToken();
-    if (token) {
-      setAccessToken(token);
-    }
-  }, []);
-
-  const setAccessToken = (accessToken: string) => {
-    setAuthorizationToken(accessToken);
-    setGlobalData({
-      ...globalData,
-      accessToken,
-    });
-  };
-
   const setNotification = (message: string, type: NotificationType, description?: string) => {
     setGlobalData({
       ...globalData,
@@ -67,8 +49,6 @@ export const useGlobalContext = () => {
 
   return {
     notification: globalData?.notification,
-    accessToken: globalData?.accessToken,
-    setAccessToken,
     setNotification,
   };
 };
--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -1,17 +1,23 @@
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { AuthType } from "../../modules/login/types/AuthType";
+import { ProductRoutesEnum } from "../../modules/product/routes";
+import { ERROR_INVALID_PASSWORD } from "../constants/errosStatus";
+import { URL_AUTH } from "../constants/urls";
+import { setAuthorizationToken } from "../functions/connection/auth";
 import { connectionAPIPost } from "../functions/connection/connectionAPI";
-// import { ConnectionAPIPost } from "../functions/connection/connectionAPI";
 import { useGlobalContext } from "./useGlobalContext";
 
 export const useRequests = () => {
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const { setNotification } = useGlobalContext();
 
   const getRequest = async (url: string) => {
     setLoading(true);
-    const returnData = await axios({
+    return await axios({
       method: "get",
       url: url,
     })
@@ -21,17 +27,13 @@ export const useRequests = () => {
       .catch(() => {
         alert("Erro");
       });
-
-    setLoading(false);
-
-    return returnData;
   };
 
   const postRequest = async <T>(url: string, body: unknown): Promise<T | undefined> => {
     setLoading(true);
+
     const returnData = await connectionAPIPost<T>(url, body)
       .then((result) => {
-        setNotification("Entrando...", "success", "aguarde");
         return result;
       })
       .catch((error: Error) => {
@@ -40,12 +42,29 @@ export const useRequests = () => {
       });
 
     setLoading(false);
-
     return returnData;
+  };
+
+  const authRequest = async (body: unknown): Promise<void> => {
+    setLoading(true);
+
+    await connectionAPIPost<AuthType>(URL_AUTH, body)
+      .then((result) => {
+        setAuthorizationToken(result.accessToken);
+        navigate(ProductRoutesEnum.PRODUCT);
+        return result;
+      })
+      .catch(() => {
+        setNotification(ERROR_INVALID_PASSWORD, "error");
+        return undefined;
+      });
+
+    setLoading(false);
   };
 
   return {
     loading,
+    authRequest,
     getRequest,
     postRequest,
   };
