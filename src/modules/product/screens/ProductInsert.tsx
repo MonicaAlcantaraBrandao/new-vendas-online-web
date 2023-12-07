a7068cb4455d0b27e75e02b4f3b88db005427798
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-12-07 19:44:36 -0300

    insert product in backend

--- src/modules/product/screens/ProductInsert.tsx
+++ src/modules/product/screens/ProductInsert.tsx
@@ -1,17 +1,24 @@
-import { Select } from "antd";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Button from "../../../shared/components/buttons/button/Button";
 import Input from "../../../shared/components/inputs/input/Input";
+import Select from "../../../shared/components/inputs/select/Select";
 import Screen from "../../../shared/components/screen/Screen";
-import { URL_CATEGORY } from "../../../shared/constants/urls";
+import { URL_CATEGORY, URL_PRODUCT } from "../../../shared/constants/urls";
+import { InsertProduct } from "../../../shared/dtos/InsertProduct.dto";
 import { MethodsEnum } from "../../../shared/enums/methods.enum";
+import { connectionAPIPost } from "../../../shared/functions/connection/connectionAPI";
 import { useDataContext } from "../../../shared/hooks/useDataContext";
 import { useRequests } from "../../../shared/hooks/useRequests";
 import { ProductRoutesEnum } from "../routes";
 import { LimitedContainer } from "../styles/productInsert.style";
 
 const ProductInsert = () => {
+  const [product, setProduct] = useState<InsertProduct>({
+    name: "",
+    price: 0,
+    image: "",
+  });
   const { categories, setCategories } = useDataContext();
   const { request } = useRequests();
 
@@ -21,8 +28,29 @@ const ProductInsert = () => {
     }
   }, []);
 
+  const handleInsertProduct = () => {
+    connectionAPIPost(URL_PRODUCT, product);
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>, nameObject: string) => {
+    setProduct({
+      ...product,
+      [nameObject]: event.target.value,
+    });
+  };
+
+  const onChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct({
+      ...product,
+      price: Number(event.target.value),
+    });
+  };
+
   const handleChange = (value: string) => {
-    console.log(`selected ${value}`);
+    setProduct({
+      ...product,
+      categoryId: Number(value),
+    });
   };
 
   return (
@@ -41,12 +69,30 @@ const ProductInsert = () => {
       ]}
     >
       <LimitedContainer>
-        <Input margin="0px 0px 16px 0px" title="Nome" placeholder="Nome" />
-        <Input margin="0px 0px 16px 0px" title="Url Imagem" placeholder="Url Imagem" />
-        <Input margin="0px 0px 16px 0px" title="preço" placeholder="preço" />
+        <Input
+          onChange={(event) => onChange(event, "name")}
+          value={product.name}
+          margin="0px 0px 16px 0px"
+          title="Nome:"
+          placeholder="Nome"
+        />
+        <Input
+          onChange={(event) => onChange(event, "image")}
+          value={product.image}
+          margin="0px 0px 16px 0px"
+          title="Url Imagem:"
+          placeholder="Url Imagem"
+        />
+        <Input
+          onChange={onChangePrice}
+          value={product.price}
+          margin="0px 0px 16px 0px"
+          title="preço:"
+          placeholder="preço"
+        />
         <Select
-          defaultValue="Selecione a categoria"
-          style={{ width: "100%" }}
+          margin="0px 0px 32px 0px"
+          title="Categoria:"
           onChange={handleChange}
           options={categories.map((category) => ({
             value: `${category.id}`,
@@ -54,7 +100,9 @@ const ProductInsert = () => {
           }))}
         />
 
-        <Button type="primary">Inserir Produto</Button>
+        <Button onClick={handleInsertProduct} type="primary">
+          Inserir Produto
+        </Button>
       </LimitedContainer>
     </Screen>
   );
